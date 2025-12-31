b9bfe2dba386dd1043a9ed8e19e573af9740c378
Mohammed <Test@gmail.com>
2025-12-31 18:25:39 -0500

    first commit

---
+++ day1_basic.js
@@ -0,0 +1,11 @@
+var message = "Hello, World!";
+console.log(message);
+var age = 25;
+console.log("Age:", age);
+// To Do:
+var isStudent = true;
+console.log("Is Student:", isStudent);
+var score = 95.5;
+console.log("Score:", score);
+var firstName = "John";
+console.log("First Name:", firstName);
---
+++ day1_basic.ts
@@ -0,0 +1,17 @@
+let message : string = "Hello, World!";
+console.log(message);
+
+let age :number = 25;
+console.log("Age:", age);
+
+// To Do:
+
+let isStudent :boolean = true;
+console.log("Is Student:", isStudent);
+
+let score : number = 95.5;
+console.log("Score:", score);
+
+let firstName : string = "John";
+console.log("First Name:", firstName);
+
