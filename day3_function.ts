016fedb53118e306dcb21bf84b3c520d78a9ec19
Mohammed <hossainm2424@gmail.com>
2026-01-01 09:52:29 -0500

    day 3

---
+++ day3_function.ts
@@ -0,0 +1,99 @@
+// Let's learn about function and object in TypeScript
+
+// Function with typed parameters and return type
+function addition(a:number, b :number):number{
+    return a+b;
+}
+let sum = addition(10,20);
+console.log("Sum is:",sum);
+
+// Arrow function is a shorter syntax for writing functions
+const multi =(x:number, y:number):number =>{
+    return x*y;
+}
+let multiresult= multi(5,10);
+console.log("Multiplication result is:",multiresult);
+
+// optional parameters in function
+// optional paramter is denoted by ? mark 
+// optional parameter must be placed after required parameter 
+
+function display(name:string, grade?:string):string{
+    if(grade){
+        return "Name: "+ name +", Grade: "+ grade;
+    }
+    return "Name: "+ name +", Grade not provided";
+}
+
+console.log(display("Alice","testing optional parameter"));
+console.log(display("Bob"));
+
+
+// Default Parameters - Value automatically assigned if no value is provided
+function countryinfo(country: string, capital:string="unknown"):string{
+    return "Country: "+ country + ", Capital: :"+ capital;
+}
+
+console.log(countryinfo("USA","Washington D.C."));
+console.log(countryinfo("Narnia"));
+
+
+// void function - does not return any value
+function logmessage (msg: string):void{
+    console.log("Log message:", msg);
+}
+console.log(logmessage("This is a void function"));
+
+
+// Typing Object 
+const student :
+{
+    name : string,
+    age : number,
+    isStudent : boolean
+
+}
+=
+{
+    name:"John", 
+    age:21,
+    isStudent:true
+}
+
+console.log("Student Info:", student);
+
+
+
+
+// Optional prperty in object type
+const employee: {
+    id: number,
+    name: string,
+    department?: string
+
+} ={
+    id:101,
+    name:"Jane Doe"
+};
+console.log("Employee Info:", employee);
+
+
+
+// readonly property in object type - prevent modification
+const car: {
+    readonly vin: string,
+    model: string
+} = {
+    vin: "1HGCM82633A123456",
+    model: "Honda Accord"
+}
+
+
+console.log("Car Info:", car);
+
+// Function with object parameter 
+function printUser(user:{name:string, email:string}):void{
+    console.log("User Name:", user.name);
+    console.log("User Email:", user.email);
+}
+
